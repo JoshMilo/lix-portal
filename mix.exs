05bf6780baafc7bcd050a2c833e19afa228e586b
Josh Milo <josh@joshmilo.com>
2016-06-10 21:51:24 -0700

    adding supervisor

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule LixPortal.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger],
+     mod: {LixPortal, []}]
   end
 
   # Dependencies can be Hex packages:
